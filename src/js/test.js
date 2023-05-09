e917f51f270098a42f2581092d05f8641bdbfca1
ivanchikap <anton.ivanchik@gmail.com>
2023-05-09 10:56:27 +0300

    add arrow to top

---
+++ src/js/test.js
@@ -0,0 +1,40 @@
+const arrow = document.querySelector('#to-top');
+const header = document.querySelector('.header');
+
+
+const headerHeight = header.clientHeight;
+let scrollOffset = document.documentElement.scrollTop;
+checkScroll(scrollOffset);
+window.addEventListener('scroll', (e) => {
+    scrollOffset = document.documentElement.scrollTop;
+
+    checkScroll(scrollOffset);
+});
+
+function checkScroll(scrollOffset) {
+    if (scrollOffset >= headerHeight) {
+        arrow.classList.remove("hidden");
+    }
+
+    if (scrollOffset < headerHeight) {
+        arrow.classList.add("hidden");
+    }
+}
+
+arrow.addEventListener('click', () => {
+    header.scrollIntoView({
+     behavior: 'smooth',
+     block: 'start',
+    });
+})
+
+
+
+// window.addEventListener('scroll', (e) => {
+//     console.log(document.documentElement.scrollTop);
+// })
+
+//element.scrollIntoView({
+//  behavior: 'smooth',
+//  block: 'end',
+//});
