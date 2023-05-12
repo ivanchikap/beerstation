9c50e7cd47080ab2ef7a1fbf35bd8180318e109b
ivanchikap <anton.ivanchik@gmail.com>
2023-05-12 13:02:57 +0300

    during main

--- src/js/sort.js
+++ src/js/sort.js
@@ -1,12 +1,8 @@
-const icons = document.querySelectorAll('.type__list-icons');
+const icons = document.querySelectorAll('.filter__sort-direction');
 
 icons.forEach((icon) => {
     icon.addEventListener('click', (e) => {
-        icons.forEach((icon) => {
-            icon.classList.remove('clicked');
-        })
         const target = e.currentTarget;
-        target.classList.add('clicked');
         const isDownPresent = target.classList.contains('down');
         const isUpPresent = target.classList.contains('up');
         if (isDownPresent) {
